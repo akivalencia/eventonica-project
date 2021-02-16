bb9b742b6d69106643109307afaa8bede20643c6
akivalencia <adilene.valencias@gmail.com>
2021-02-16 15:56:44 -0800

    added more to the user and event classes

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 class Eventonica {
     addEvent(/* arguments you decide go here */) {
+    let newEvent= new Event();
+    
       // Adds a new Event
     }
   
@@ -7,29 +9,48 @@ class Eventonica {
       // Update existing Event
     }
   
-    deleteEvent() {
+    deleteEvent(id) {
       // Deletes Event
     }
   
-    findEventsByDate() {
+    findEventsByDate(date) {
       // Return items in Event.all with a specified date
     }
   
-    findEventsbyCategory() {
+    findEventsbyCategory(category) {
       // Return items in Event.all with a specified category
     }
   
-    addUser() {
+    addUser(id) {
       // Adds a new User
+      //pops the user from the array 
     }
   
     updateUser() {
       // Update existing User
     }
   
-    deleteUser() {
+    deleteUser(id) {
       // Deletes User
+    for (let i=0; i<User.all.length; i++) {
+         if (User.all[i].id === id){
+          User.all.splice(i,1);
+
+        }
+      }   
     }
+
+
+    favoriteEvent() {
+        //favoritesEvent
+    }
+
+    unfavoriteEvent() {
+        //undoes the favoriteEvent method
+    }
+
+
+
   }
   
   class Event {
@@ -39,6 +60,11 @@ class Eventonica {
     constructor() {
       this.id = Event._nextId++;
       // decide what properties are required
+      this.title= title;
+      this.date= date;
+      this.organizer= organizer;
+      this.description= description;
+      this.category= category;
       Event.all.push(this); // keep track of all created instances
     }
   
@@ -57,7 +83,20 @@ class Eventonica {
   
     constructor() {
       this.id = User._nextId++;
+      this.first_name= first_name;
+      this.last_name= last_name;
+      this.email= email;
+      this.cateorgy= category;
+
       // decide what properties are required on an instance
       User.all.push(this); // keep track of all created instances
     }
   }
+
+//can create categories in general whether it's favorite or not
+ class Category{
+    constructor(){
+        this.name= name;
+    }
+
+ }
