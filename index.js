dc72ab35029f6075e53a4fd4c710da46ce149f86
akivalencia <adilene.valencias@gmail.com>
2021-02-17 09:37:16 -0800

    added more methods to eventonica class

--- index.js
+++ index.js
@@ -1,40 +1,69 @@
 class Eventonica {
-    addEvent(/* arguments you decide go here */) {
-    let newEvent= new Event();
-    
+    addEvent(title, date, organizer, description, category) {
+    let newEvent= new Event(title, date, organizer, description, category);
       // Adds a new Event
     }
-  
-    updateEvent() {
+    updateEvent(id, name, key, change) {
       // Update existing Event
+      //search id 
+      //value 
+      //change key 
+      //change value --> key value pairs 
+      //map or filter methods 
     }
   
     deleteEvent(id) {
       // Deletes Event
-    }
+      for (let i=0; i<Event.all_users.length; i++) {
+        if (Event.all_events[i].id === id){
+         Event.all_events.splice(i,1);// what truly is this splice shit doing 
+
+       }
+     }   
+   }
+
   
     findEventsByDate(date) {
       // Return items in Event.all with a specified date
+      //use filter method--> that honeslty seems like the easiest thing to do 
+      //let item = event.all_event[i] == one event in the array 
+      //event.all_event[i]["date"] ; the date key/ property
+      //if event.all_event[i]["date"]=== date 
+    //   return item ;
+
+      
+
+       }
     }
   
     findEventsbyCategory(category) {
       // Return items in Event.all with a specified category
+      //use filter method
     }
   
     addUser(id) {
       // Adds a new User
+        let newUser= new User(id, first_name, last_name, email, category, password);
+          // Adds a new Event
+        }
       //pops the user from the array 
     }
   
-    updateUser() {
-      // Update existing User
+    updateUser(id, name, key, change) {
+        // Update existing Event
+        //search id 
+        //value 
+        //change key 
+        //change value --> key value pairs 
+        //map or filter methods 
+
     }
   
     deleteUser(id) {
       // Deletes User
-    for (let i=0; i<User.all.length; i++) {
-         if (User.all[i].id === id){
-          User.all.splice(i,1);
+    for (let i=0; i<User.all_users.length; i++) {
+         if (User.all_users[i].id === id){
+          User.all_users.splice(i,1);// what truly is this splice shit doing 
 
         }
       }   
@@ -54,10 +83,10 @@ class Eventonica {
   }
   
   class Event {
-    static all = [];
+    static all_events = [];
     static _nextId = 100;
   
-    constructor() {
+    constructor(title, date, organizer, description, category) {
       this.id = Event._nextId++;
       // decide what properties are required
       this.title= title;
@@ -65,7 +94,7 @@ class Eventonica {
       this.organizer= organizer;
       this.description= description;
       this.category= category;
-      Event.all.push(this); // keep track of all created instances
+      Event.all_events.push(this); // keep track of all created instances
     }
   
     static findByDate() {
@@ -78,25 +107,28 @@ class Eventonica {
   }
   
   class User {
-    static all = [];
+    static all_users= [];
     static _nextId = 200;
   
+
+
     constructor() {
       this.id = User._nextId++;
       this.first_name= first_name;
       this.last_name= last_name;
       this.email= email;
-      this.cateorgy= category;
+      this.category= category;
+      this.password= password;
 
       // decide what properties are required on an instance
-      User.all.push(this); // keep track of all created instances
+      User.all_users.push(this); // keep track of all created instances
     }
   }
 
-//can create categories in general whether it's favorite or not
- class Category{
-    constructor(){
-        this.name= name;
-    }
+// //can create categories in general whether it's favorite or not
+//  class Category{
+//     constructor(){
+//         this.name= name;
+//     }
 
- }
+//  }
